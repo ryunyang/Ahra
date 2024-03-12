36cda31424b498321343c449bb7010ad965b7e28
ryunyang <rsh000325@naver.com>
2024-03-12 21:43:22 +0900

    test2

---
+++ Pioneer/build/pioneer/CMakeFiles/pioneer.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "/home/ryu/Pioneer/devel/lib/libpioneer.pdb"
+  "/home/ryu/Pioneer/devel/lib/libpioneer.so"
+  "CMakeFiles/pioneer.dir/src/Walkingpattern_generator.cc.o"
+  "CMakeFiles/pioneer.dir/src/pioneer_plugin.cc.o"
+  "CMakeFiles/pioneer.dir/src/test.cc.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/pioneer.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
