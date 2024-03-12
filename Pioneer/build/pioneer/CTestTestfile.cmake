27cbcb406e221dcc73706874308a31922640f608
ryunyang <rsh000325@naver.com>
2024-03-12 21:42:29 +0900

    delete

--- Pioneer/build/pioneer/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/ryu/Pioneer/src/pioneer
-# Build directory: /home/ryu/Pioneer/build/pioneer
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- Pioneer/build/pioneer_description3/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/ryu/Pioneer/src/pioneer_description3
-# Build directory: /home/ryu/Pioneer/build/pioneer_description3
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
